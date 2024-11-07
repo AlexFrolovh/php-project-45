cacf2c005776f38c859f246819c6adb067a413ed
Alex <alexfrolovyh@list.ru>
2024-11-07 20:05:34 +0300

    Update Even.php

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -4,6 +4,7 @@ namespace BrainGames\Games\Even;
 
 use function cli\line;
 use function cli\prompt;
+use function BrainGames\src\Engine;
 
 const ROUND_COUNT = 3;
 
@@ -14,26 +15,10 @@ function isEven($number)
 
 function runGameEven()
 {
+  $gameRules = "Answer \"yes\" if the number is even, otherwise answer \"no\"."
+  $randomNumber = rand(0, 100);
+  $rightAnswer = isEven($randomNumber)? 'yes' : 'no';
 
-  line('Welcome to the Brain Game!');
-  $name = prompt('May I have your name?');
-  line("Hello, %s!", $name);
-
-  line("Answer \"yes\" if the number is even, otherwise answer \"no\".");
-
-  for ($i = 0; $i < ROUND_COUNT; $i++) {
-    $randomNumber = rand(0, 100);
-    $rightAnswer = isEven($randomNumber)? 'yes' : 'no';
-
-    line("Question: {$randomNumber}");
-    $answer = prompt('Your answer');
-    if ($rightAnswer === $answer) {
-      line("Correct!");
-    } else {
-      line("\"{$answer}\" is wrong answer ;(. Correct answer was \"{$rightAnswer}\".");
-      line("Let's try again, {$name}!");
-      return;
-    }
-  }
-  line("Congratulations, {$name}!");
+  line("Question: {$randomNumber}");
+  engine();
 }
