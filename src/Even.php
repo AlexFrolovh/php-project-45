af0de976b8786918a1ca45bf5a22c52b16b4241a
Alex <alexfrolovyh@list.ru>
2024-11-07 19:26:15 +0300

    Update Even.php

--- src/Even.php
+++ src/Even.php
@@ -5,49 +5,35 @@ namespace BrainGames\Even;
 use function cli\line;
 use function cli\prompt;
 
-const ITERATIONS_COUNT = 3;
+const ROUND_COUNT = 3;
 
 function isEven($number)
 {
     return $number % 2 === 0;
 }
 
-function runEngine($getRightAnswerForRound, $question)
+function runGameEven()
 {
-    line('Welcome to the Brain Game!');
-    line($question);
-    $name = prompt('May I have your name?');
-    line('Hello, %s!', $name);
 
-    for ($i = 0; $i < ITERATIONS_COUNT; $i++) {
-        ['roundQuestion' => $roundQuestion, 'rightAnswer' => $rightAnswer] = $getRightAnswerForRound();
-        
-        line("Question: {$roundQuestion}");
-        $answerUser = prompt('Your answer');
-        
-        if ($answerUser == $rightAnswer) {
-            line('Correct!');
-        } else {
-            line("'{$answerUser}'  is wrong answer ;(. Correct answer was '{$rightAnswer}'.");
-            line("Let's try again, {$name}!");
-            return;
-        }
+  line('Welcome to the Brain Game!');
+  $name = prompt('May I have your name?');
+  line("Hello, %s!", $name);
+
+  line("Answer \"yes\" if the number is even, otherwise answer \"no\".\n");
+
+  for ($i = 0; $i < ROUND_COUNT; $i++) {
+    $randomNumber = rand(0, 100);
+    $rightAnswer = isEven($randomNumber)? 'yes' : 'no';
+
+    line("Question: {$randomNumber}\n");
+    $answer = prompt('Your answer:');
+    if ($rightAnswer === $answer) {
+      line("Correct!\n");
+    } else {
+      line("\"{$answer}\" is wrong answer ;(. Correct answer was \"{$rightAnswer}\".\n");
+      line("Let's try again, {$name}!");
+      return;
     }
-    
-    line("Congratulations, {$name}!");
-    return;
-}
-
-function runEvenGame()
-{
-  $getRightAnswerForRound = function () {
-  $randomTopNumber = 100;
-  $number = rand(0, $randomTopNumber);
-  $rightAnswer = isEven($number) ? "yes" : "no";
-  $roundQuestion = $number;
-    
-  return ['roundQuestion' => $roundQuestion, 'rightAnswer' => $rightAnswer];
-  };
-    
-  runEngine($getRightAnswerForRound, 'Answer "yes" if the number is even, otherwise answer "no".');
+  }
+  line("Congratulations, {$name}!");
 }
