460bb681f06e38daf55ba18e967d0f10ba1c11c2
Alex <alexfrolovyh@list.ru>
2024-11-07 20:02:35 +0300

    Update Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -1,1 +1,36 @@
+<?php
 
+namespace BrainGames;
+
+use function cli\line;
+use function cli\prompt;
+
+const ROUND_COUNT = 3;
+
+function isEven($number)
+{
+    return $number % 2 === 0;
+}
+
+function engine()
+{
+
+  line('Welcome to the Brain Game!');
+  $name = prompt('May I have your name?');
+  line("Hello, %s!", $name);
+  line($gameRules);
+
+  for ($i = 0; $i < ROUND_COUNT; $i++) {
+
+    line("Question: {$randomNumber}");
+    $answer = prompt('Your answer:');
+    if ($rightAnswer === $answer) {
+      line("Correct!");
+    } else {
+      line("\"{$answer}\" is wrong answer ;(. Correct answer was \"{$rightAnswer}\".");
+      line("Let's try again, {$name}!");
+      return;
+    }
+  }
+  line("Congratulations, {$name}!");
+}
