5d96fae5fe9c2693a7a098720a099946dbf38e1d
Alex <alexfrolovyh@list.ru>
2024-11-07 21:17:21 +0300

    Update Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace BrainGames\Engine;
+namespace BrainGames;
 
 use function cli\line;
 use function cli\prompt;
@@ -26,4 +26,5 @@ function engine($gameRules,$randomNumber,$rightAnswer)
     }
   }
   line("Congratulations, {$name}!");
+  return;
 }
