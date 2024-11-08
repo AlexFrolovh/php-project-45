9658668f00908f5a0965b37be4eb4a9cbf163c10
Alex <alexfrolovyh@list.ru>
2024-11-08 22:23:50 +0300

    Update Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -9,19 +9,19 @@ use function BrainGames\Games\invert;
 
 const ROUND_COUNT = 3;
 
-function engine($gameRules, $randomNumbers, $answer)
+function engine($gameRules, $randomNumbers, $answer, $index)
 {
   line('Welcome to the Brain Game!');
   $name = prompt('May I have your name?');
   line("Hello, %s!", $name);
   line($gameRules);
 
-  for ($i = 0; $i < ROUND_COUNT;) {
-    line("Question: {$randomNumbers[$i]}");
+  for ($i = 0; $i < ROUND_COUNT; $i++;) {
+    line("Question: {$randomNumbers[$index]}");
     $answer = prompt('Your answer');
     if (getRightAnswer($randomNumbers) === $answer) {
+      $index++;
       line("Correct!");
-      $i++;
     } else {
       line("\"{$answer}\" is wrong answer ;(. Correct answer was " . '"' . invert($answer) . '".' );
       line("Let's try again, {$name}!");
