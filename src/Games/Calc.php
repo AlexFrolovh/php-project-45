637cfe58986d669fa66af86e9795de83828ce1bb
Alex <alexfrolovyh@list.ru>
2024-11-09 20:41:01 +0300

    Update Calc.php

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -7,7 +7,7 @@ use function BrainGames\engine;
 function runGameCalc()
 {
     $gameRules = "What is the result of the expression?";
-    $question = [rand(0, 10) + rand(0, 10), rand(0, 10) - rand(0, 10), rand(0, 10) * rand(0, 10)]
+    $question = [rand(0, 10) + rand(0, 10), rand(0, 10) - rand(0, 10), rand(0, 10) * rand(0, 10)];
     $
     engine($gameRules, $);
 }
