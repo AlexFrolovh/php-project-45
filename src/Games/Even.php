6b30ae212fa96e30b8ecb896ab724923df8ffca8
Alex <alexfrolovyh@list.ru>
2024-11-09 20:44:41 +0300

    Update Even.php

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -13,6 +13,8 @@ function runGameEven()
 {
     $gameRules = "Answer \"yes\" if the number is even, otherwise answer \"no\".";
     $question = [rand(0, 100), rand(0, 100), rand(0, 100)];
+    $index = rand(0, 2);
+    $rightAnswer = isEven($question[$index]) ? 'yes' : 'no';
     
-    engine($gameRules, $question);
+    engine($gameRules, $question, $index, $rightAnswer);
 }
