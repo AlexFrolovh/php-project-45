90a5fcb15fa1c77a649e7627ccdf1670ee292bf6
Alex <alexfrolovyh@list.ru>
2024-11-09 20:55:49 +0300

    Update Even.php

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -9,12 +9,17 @@ function isEven($number)
     return ($number % 2 === 0);
 }
 
+function rightAnswer($number)
+{
+    return isEven($number) ? 'yes' : 'no';
+}
+
 function runGameEven()
 {
     $gameRules = "Answer \"yes\" if the number is even, otherwise answer \"no\".";
     $question = [rand(0, 100), rand(0, 100), rand(0, 100)];
-    $index = 0;
-    $rightAnswer = isEven($question[$index]) ? 'yes' : 'no';
+    #$index = 0
+    #$rightAnswer = isEven($question[$index]) ? 'yes' : 'no';
     
-    engine($gameRules, $question, $index, $rightAnswer);
+    engine($gameRules, $question);
 }
