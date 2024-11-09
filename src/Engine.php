e0b339c71794516062c3b2211bf016c2b63808f3
Alex <alexfrolovyh@list.ru>
2024-11-09 20:06:25 +0300

    Update Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -8,7 +8,7 @@ use function BrainGames\Games\isEven;
 
 const ROUND_COUNT = 3;
 
-function engine()
+function engine($gameRules, $answer, $question, $rightAnswer, $index)
 {
   line('Welcome to the Brain Game!');
   $name = prompt('May I have your name?');
