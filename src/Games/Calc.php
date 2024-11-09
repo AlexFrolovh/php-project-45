958cf2058c1cc5918c2ecb043a844378b976c5b3
Alex <alexfrolovyh@list.ru>
2024-11-09 20:45:55 +0300

    Update Calc.php

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -1,13 +1,3 @@
 <?php
 
 namespace BrainGames\Games;
-
-use function BrainGames\engine;
-
-function runGameCalc()
-{
-    $gameRules = "What is the result of the expression?";
-    $question = [rand(0, 10) + rand(0, 10), rand(0, 10) - rand(0, 10), rand(0, 10) * rand(0, 10)];
-    $
-    engine($gameRules, $);
-}
